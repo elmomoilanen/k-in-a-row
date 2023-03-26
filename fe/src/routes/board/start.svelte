0fd25c83ac79f6046cba6f3323155a7562c6451a
Elmo Moilanen <49366097+elmomoilanen@users.noreply.github.com>
2023-03-26 16:45:15 +0300

    Update starter view responsivity settings

--- fe/src/routes/board/start.svelte
+++ fe/src/routes/board/start.svelte
@@ -19,17 +19,22 @@
         top: 0%;
         width: 100%;
         z-index: 1;
-        height: 20vh;
+        height: 30%;
         justify-content: center;
     }
     div p {
         color: white;
-        font-size: 3em;
+        font-size: 3rem;
         font-weight: bold;
         display: flex;
         justify-content: center;
+        overflow-y: clip;
     }
-
+    @media screen and (max-height: 500px) {
+        div {
+            height: 100%;
+        }
+    }
     @media (prefers-color-scheme: dark) {
         div {
             background-color: #131313;
